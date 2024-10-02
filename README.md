323074e2c998ff23bb9000c947d140d8ba3f7261
Kanmani <94244306+k890m@users.noreply.github.com>
2024-10-02 18:52:38 -0400

    Basic Structure

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 2D-Game-Engine
+
+## Features
+- 2D Sprite Rendering using SFML
+- Physics Integration using Box2D
+- Entity-Component System
+- Game Loop
+
+## Getting Started
+
+### Prerequisites
+- C++17 (or later)
+- SFML 2.5.1 or higher
+- Box2D 2.4.1 or higher
+
---
+++ src/GameEngine.h
@@ -0,0 +1,23 @@
+// src/GameEngine.h
+
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
+
+#include <SFML/Graphics.hpp>
+#include <box2d/box2d.h>
+
+class GameEngine {
+public:
+    GameEngine();
+    void run();
+
+private:
+    void processInput();
+    void update();
+    void render();
+
+    sf::RenderWindow window;
+    b2World physicsWorld;
+};
+
+#endif
